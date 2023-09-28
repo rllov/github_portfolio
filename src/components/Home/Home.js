cff27fcfe733de9963584dc203faf9b785b32987
Ryan Lov <r.lee.lov@gmail.com>
2023-09-28 11:19:03 -0700

    updated home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,36 @@
 import React from "react";
+import Typewriter from "typewriter-effect";
+import "./Home.css";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <div className="img">
+        <img src="/profile.png" alt="me"></img>
+      </div>
+
+      <div className="detail_container">
+        <h1>
+          <Typewriter
+            onInit={(typewriter) => {
+              typewriter
+                .typeString("Hello")
+                .pauseFor(1000)
+                .typeString("!")
+                .start();
+            }}
+          />
+        </h1>
+        <div className="title">
+          <h1>My Name is Ryan Lov</h1>
+        </div>
+        <p>
+          I am interested in becoming a front-end software engineer. I enjoy
+          creating code that users can see and interact with.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
